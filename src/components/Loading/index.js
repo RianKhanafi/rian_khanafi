c2325028847c6298dee61ab3fafd6da132fe3505
Rian Khanafi <51011550+RianKhanafi@users.noreply.github.com>
2021-10-22 16:20:46 +0700

    add Loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+export default function Loading() {
+  return (
+    <div className="loading">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
