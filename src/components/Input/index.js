fde59edcf5b73c9536972a4dc07be090405051f3
Rian Khanafi <51011550+RianKhanafi@users.noreply.github.com>
2021-10-22 16:18:50 +0700

    add input onchnage and className custom

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./input.css";
 
-export default function Input({ className, placeholder }) {
+export default function Input({ className, placeholder, onChange }) {
   return (
     <div className={["input-container", className].join(" ")}>
-      <input type="text" placeholder={placeholder} />
+      <input type="text" placeholder={placeholder} onChange={onChange} />
     </div>
   );
 }
