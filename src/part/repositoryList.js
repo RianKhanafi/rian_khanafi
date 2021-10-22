28f21e09e69ea53a73951b129d82893587e7ef21
Rian Khanafi <51011550+RianKhanafi@users.noreply.github.com>
2021-10-23 00:24:01 +0700

    update api profile for get total of repository, and adding pagination

--- src/part/repositoryList.js
+++ src/part/repositoryList.js
@@ -5,26 +5,53 @@ import { ReactComponent as IconFile } from "assets/icon/file.svg";
 import Input from "components/Input";
 import Loading from "components/Loading";
 import Error from "components/Error";
+import Paging from "components/Paging";
 const RepositoryList = ({ serach }) => {
   const [fetching, setFetching] = useState({
     loading: false,
     data: [],
     error: null,
   });
+
+  const [filter, setFilter] = useState("");
+
+  const [page, setPage] = useState({
+    current: 1,
+    total: 0,
+  });
+
   useEffect(() => {
-    getAllData();
+    getUserRepoDetail();
   }, [, serach]);
+
+  useEffect(() => {
+    if (page.total) getAllData(page);
+  }, [, serach, page]);
 
   const getAllData = async () => {
     const username = serach || "RianKhanafi";
 
-    setFetching({ loading: true, data: [], error: null });
+    setFetching({ ...fetching, loading: true, error: null });
     try {
-      const data = await GithubApi.getAll(username);
+      const data = await GithubApi.getAll(username, { page: page.current });
       setFetching({ loading: false, data: data, error: null });
     } catch (error) {
       setFetching({ loading: false, data: [], error: error });
     }
+  };
+
+  const getUserRepoDetail = async () => {
+    const username = serach || "RianKhanafi";
+    try {
+      const userRepos = await GithubApi.profile(username);
+
+      setPage((old) => {
+        return {
+          ...old,
+          total: userRepos?.public_repos,
+        };
+      });
+    } catch (err) {}
   };
 
   return (
@@ -34,20 +61,41 @@ const RepositoryList = ({ serach }) => {
         Repositories <span> {(fetching?.data ?? []).length}</span>
       </h1>
 
-      <Input className="input" placeholder="Find a repository..." />
-      {fetching.loading && <Loading />}
+      <Input
+        className="input"
+        placeholder="Find a repository..."
+        onChange={(e) => setFilter(e.target.value)}
+      />
       {fetching.error && fetching.data.length === 0 && <Error />}
-      {(fetching?.data ?? []).map((elm, index) => {
-        return (
-          <Card
-            key={index}
-            name={elm?.name}
-            visibility={elm.visibility}
-            language={elm?.language}
-            watchers={elm?.watchers}
-          />
-        );
-      })}
+      {!fetching?.data?.length && !fetching?.loading && !fetching.error && (
+        <span>No Repository</span>
+      )}
+      {(fetching?.data ?? [])
+        .filter((e) => e.name.includes(filter))
+        .map((elm, index) => {
+          return (
+            <Card
+              key={index}
+              name={elm?.name}
+              visibility={elm.visibility}
+              language={elm?.language}
+              watchers={elm?.watchers}
+            />
+          );
+        })}
+      {fetching.loading && <Loading />}
+      <Paging
+        total={page.total}
+        current={page.current}
+        onChange={(e) => {
+          setPage((old) => {
+            return {
+              ...old,
+              current: e,
+            };
+          });
+        }}
+      />
     </div>
   );
 };
