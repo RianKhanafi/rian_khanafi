a34494c1c30f54d72a79dc5d6f92b828722cf9a8
Rian Khanafi <51011550+RianKhanafi@users.noreply.github.com>
2021-10-22 17:42:59 +0700

    add Result from : {search} caption

--- src/part/githubProfile.js
+++ src/part/githubProfile.js
@@ -1,11 +1,14 @@
 import Input from "components/Input";
 import React, { useState } from "react";
 
-export default function GithubProfile({ hanleSearch }) {
+export default function GithubProfile({ hanleSearch, serach }) {
   return (
     <div>
       <h3>Find Repository by username</h3>
       <Input placeholder="Github username" onChange={hanleSearch} />
+      <span>
+        <i>Result from: {serach}</i>
+      </span>
     </div>
   );
 }
