0d450533dd04036287acf584405868f82f1920a2
Rian Khanafi <51011550+RianKhanafi@users.noreply.github.com>
2021-10-22 16:15:14 +0700

    add leftside content props

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./layout.css";
 
-export default function Layout({ children }) {
+export default function Layout({ children, leftcontent }) {
   return (
     <div className="layout">
       <aside className="left-side">
-        <div className="left-content"></div>
+        <div className="left-content">{leftcontent}</div>
       </aside>
       <section className="right-side">
         <div className="right-content">{children}</div>
