870ac4e1bacff626d5398d4e94f574d969478762
Rian Khanafi <51011550+RianKhanafi@users.noreply.github.com>
2021-10-22 17:43:54 +0700

    add components loading

---
+++ src/components/Error/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Error() {
+  return (
+    <div>
+      <span style={{ color: "#C1004B" }}>Repositories username not found!</span>
+    </div>
+  );
+}
